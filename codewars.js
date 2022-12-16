07993b0d1e0e68b3e11d1d7480d3814605219d1f
javialroro <102406440+javialroro@users.noreply.github.com>
2022-12-15 23:21:01 -0600

    Codewars exercise

---
+++ codewars.js
@@ -0,0 +1,29 @@
+function solution(start, finish) 
+{
+    if (Number.isInteger(start)===false && Number.isInteger(finish)===false){
+        return console.log("Value must be a number");
+     }
+    if (start < 0 && finish < 0){
+    return console.log("The number must be positive");
+  }
+    if (start > finish){
+        return console.log("Start must be bigger");
+    }
+    var saltos = 0;
+    while (start <= finish){
+        if (start + 3 < finish){
+            start += 3;
+        }
+        else{
+            start ++;
+        }
+        saltos ++;
+  }
+  return saltos
+}
+
+
+//solution(1,5)
+
+
+console.log(solution(1,5))
